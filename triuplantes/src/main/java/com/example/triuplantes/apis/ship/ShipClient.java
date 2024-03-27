38cf39d01e3dfa4cf8e541cb54458fe7343ad5ca
Mrkaponi <mrkaponi@outlook.es>
2024-03-27 16:52:18 -0600

    update
    update

--- triuplantes/src/main/java/com/example/triuplantes/apis/ship/ShipClient.java
+++ triuplantes/src/main/java/com/example/triuplantes/apis/ship/ShipClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient( url = "http://localhost:8080/ships/")
+@FeignClient( name = "ship",url = "http://localhost:8080/ships/")
 public interface ShipClient {
 
     @GetMapping("{id}")
